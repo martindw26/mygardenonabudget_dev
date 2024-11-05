34e429ff439a3013c20286bc047252bf9654b347
martin williams <86750566+martindw26@users.noreply.github.com>
2024-11-05 22:54:59 +0000

    update related

--- includes/related.php
+++ includes/related.php
@@ -3,45 +3,18 @@
 $current_category = get_the_category();
 $current_category_id = !empty($current_category) ? $current_category[0]->term_id : null;
 
-// Set up initial query arguments
+// Set up initial query arguments to filter by category only
 $args = [
     'post_type'      => 'post',
     'posts_per_page' => $min_results,
     'category__in'   => $current_category_id ? [$current_category_id] : [], // Filter by current category
-    'meta_query'     => [
-        'relation' => 'AND',
-        [
-            'key'     => 'make',
-            'value'   => $make,
-            'compare' => 'LIKE',
-        ],
-        [
-            'key'     => 'model',
-            'value'   => $model,
-            'compare' => 'LIKE',
-        ],
-    ],
 ];
 
 $query = new WP_Query($args);
 
 // If not enough results, modify query to search by keywords
 if ($query->found_posts < $min_results && !empty($keywords)) {
-    $args['meta_query'] = [
-        'relation' => 'OR',
-        [
-            'key'     => 'make',
-            'value'   => $make,
-            'compare' => 'LIKE',
-        ],
-        [
-            'key'     => 'model',
-            'value'   => $model,
-            'compare' => 'LIKE',
-        ],
-    ];
-    $args['s'] = $keywords; // Search by keywords in content or description
-
+    $args['s'] = $keywords; // Use 's' to search keywords in post content or description
     $query = new WP_Query($args);
 }
 
