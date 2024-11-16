79731542c98a9fdcb91a7a192460110fb13a59bd
martin williams <86750566+martindw26@users.noreply.github.com>
2024-11-16 16:01:57 +0000

    revert

--- front-page.php
+++ front-page.php
@@ -1,4 +1,4 @@
-<div class="container main-content">
+
 <?php
 get_header();
 /* Template Name: Home Page */
@@ -351,6 +351,5 @@ if ($leaderboard_bottom_script_enabled === 'on') {
 }
 ?>
 
-</div>
 </div>
 <?php get_footer(); ?>
