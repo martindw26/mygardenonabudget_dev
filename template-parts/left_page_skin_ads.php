704424ce1cd38c0ad992b16c972f0851efad1f81
martin williams <86750566+martindw26@users.noreply.github.com>
2024-11-17 19:02:50 +0000

    ad rotatoe left page skin v10

--- template-parts/left_page_skin_ads.php
+++ template-parts/left_page_skin_ads.php
@@ -14,37 +14,46 @@ if (!function_exists('have_rows')) {
 <div class="page-skin-left">
   <?php 
   // Fetch ads from the 'left_page_skin_ads' repeater field on the Options Page
-  if (have_rows('left_page_skin_ads', 'option')) : ?>
-    <p class="advert_label">Advertisement</p>
+  if (have_rows('left_page_skin_ads', 'option')) : 
     
-    <?php while (have_rows('left_page_skin_ads', 'option')) : the_row(); 
+    // Create an array to hold all the ad data
+    $ads = [];
+    
+    // Loop through all the repeater rows and store the data
+    while (have_rows('left_page_skin_ads', 'option')) : the_row(); 
       
       // Get subfields from the repeater
       $left_page_skin = get_sub_field('left_page_skin'); 
       $left_page_skin_url = get_sub_field('left_page_skin_url'); 
       
-      // Debugging output
-      if (!$left_page_skin) {
-        echo '<!-- No image found in left_page_skin subfield -->';
-      }
-      if (!$left_page_skin_url) {
-        echo '<!-- No URL found in left_page_skin_url subfield -->';
+      // Ensure both the image and URL are available before adding to the array
+      if ($left_page_skin && $left_page_skin_url) {
+        $ads[] = [
+          'image' => $left_page_skin,
+          'url' => $left_page_skin_url
+        ];
       }
       
-      // Check if both image and URL are available before displaying
-      if ($left_page_skin && $left_page_skin_url) : 
-        // Randomly generate a rotation angle between -10 and 10 degrees
-        $random_rotation = rand(-10, 10); 
-        ?>
-        <a href="<?php echo esc_url($left_page_skin_url); ?>" target="_blank">
-          <img src="<?php echo esc_url($left_page_skin); ?>" alt="Left Page Skin Ad">
-        </a>
-      <?php else: ?>
-        <!-- Debugging: Missing data -->
-        <p>Ad data is incomplete. Please check ACF fields on the Options Page.</p>
-      <?php endif; ?>
+    endwhile;
+
+    // If there are ads in the array, randomly select one
+    if (!empty($ads)) :
+      // Shuffle the ads array to randomize the order and select the first one
+      $random_ad = $ads[array_rand($ads)];
       
-    <?php endwhile; ?>
+      // Randomly generate a rotation angle between -10 and 10 degrees
+      $random_rotation = rand(-10, 10); 
+      ?>
+      
+      <p class="advert_label">Advertisement</p>
+      <a href="<?php echo esc_url($random_ad['url']); ?>" target="_blank" style="transform: rotate(<?php echo $random_rotation; ?>deg);">
+        <img src="<?php echo esc_url($random_ad['image']); ?>" alt="Left Page Skin Ad">
+      </a>
+
+    <?php else: ?>
+      <!-- No valid ads found in the repeater field -->
+      <p>No valid ads found. Please check the ACF fields on the Options Page.</p>
+    <?php endif; ?>
     
   <?php else : ?>
     <!-- No ads found in the repeater field on Options Page -->
